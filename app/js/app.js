f7575e4bc9f839c943b8c504eba4544b02b7771d
kunqian <kunqian@kunqians-Mac-Pro.local>
2015-01-03 11:29:43 -0800

    checkpoint, login page is now fully working, will add registration next

--- app/js/app.js
+++ app/js/app.js
@@ -1,21 +1,76 @@
 'use strict'
 var ladderApp = angular.module('ladderApp', [
     'ngRoute', 
+    'ngResource', 
     'ladderControllers'
     ]);
 
-ladderApp.config(['$routeProvider',
-    function ($routeProvider) {
+ladderApp.config(['$routeProvider', '$locationProvider', '$httpProvider', 
+    function ($routeProvider, $locationProvider, $httpProvider) {
+        
+        var checkLoggedin = function($q, $timeout, $http, $location, $rootScope){
+            // Initialize a new promise
+            var deferred = $q.defer();
+
+            // Make an AJAX call to check if the user is logged in
+            $http.get('/loggedin').success(function(user){
+                // Authenticated
+                if (user !== '0') {
+                    $timeout(deferred.resolve, 0);
+                    $rootScope.user = user.name;
+                }
+                // Not Authenticated
+                else {
+                    $rootScope.message = 'You need to log in.';
+                    $timeout(function(){deferred.reject();}, 0);
+                    $location.url('/login');
+                }
+            });
+
+            return deferred.promise;
+        };
+
+        //================================================
+        // Add an interceptor for AJAX errors
+        //================================================
+        $httpProvider.interceptors.push(function($q, $location) {
+          return function(promise) {
+            return promise.then(
+              // Success: just return the response
+              function(response){
+                return response;
+              }, 
+              // Error: check the error status to get only the 401
+              function(response) {
+                if (response.status === 401)
+                  $location.url('/login');
+                return $q.reject(response);
+              }
+            );
+          }
+        });
+
         $routeProvider.
-            when('/players', {
+            when('/standings', {
+                templateUrl: 'partials/standing-list.html',
+                controller: 'PlayerListCtrl',
+            })
+            .when('/players', {
                 templateUrl: 'partials/player-list.html',
-                controller: 'PlayerListCtrl'
-            }).
-            when('/players/:playerId', {
+                controller: 'PlayerListCtrl',
+                resolve: {
+                  loggedin: checkLoggedin
+                }
+            })
+            .when('/players/:playerId', {
                 templateUrl: 'partials/player-detail.html',
                 controller: 'PlayerDetailCtrl'
-            }).
-            otherwise({
-                redirectTo: '/players'
+            })
+            .when('/login', {
+                templateUrl: 'views/login.html',
+                controller: 'LoginCtrl'
+            })
+            .otherwise({
+                redirectTo: '/standings'
             });
     }]);
--- server.js
+++ server.js
@@ -1,13 +1,61 @@
 var express = require('express');
 var bodyParser = require('body-parser')
+var config = require('./config');
+var passport = require('passport');
+var http = require('http');
+var LocalStrategy = require('passport-local').Strategy;
+var cookieParser = require('cookie-parser');
+var morgan = require('morgan');
+var session = require('express-session');
+
+
+//==================================================================
+// Define the strategy to be used by PassportJS
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    if (username === "admin" && password === "admin") // stupid example
+        return done(null, {name: "admin"});
+
+    return done(null, false, { message: 'Incorrect username.' });
+  }
+));
+
+// Serialized and deserialized methods when got from session
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+
+// Middleware function to be used for every secured routes
+var auth = function(req, res, next){
+    if (!req.isAuthenticated())
+        res.send(401);
+    else
+        next();
+};
 
 var app = express();
+
+app.set('port', process.env.PORT || 5000);
+app.use(morgan('dev'));
+app.use(cookieParser()); 
+app.use(session({ 
+    secret: 'securedsession',
+    resave: false,
+    saveUninitialized: true 
+}));
+app.use(passport.initialize()); // Add passport initialization
+app.use(passport.session());    // Add passport initialization
 
 app.use(express.static(__dirname + '/app'));
     //app.use(express.favicon());
 app.use(bodyParser.json());
 
-var config = require('./config');
+
 app.set('dbUrl', config.db[app.settings.env]);
 
 require('angoose').init(app, {
@@ -15,4 +63,28 @@ require('angoose').init(app, {
     'mongo-opts': app.get('dbUrl'),
 });
 
-app.listen(process.env.PORT || 5000);
+
+app.get('/', auth, function(req, res){
+    res.render('index', { title: 'Express' });
+});
+
+//==================================================================
+// route to test if the user is logged in or not
+app.get('/loggedin', function(req, res) {
+  res.send(req.isAuthenticated() ? req.user : '0');
+});
+
+// route to log in
+app.post('/login', passport.authenticate('local'), function(req, res) {
+  res.send(req.user);
+});
+
+// route to log out
+app.post('/logout', function(req, res){
+  req.logOut();
+  res.send(200);
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
